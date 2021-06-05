65de7c7a07a1b337e62fb1343dc686d5e1ca7cf1
grzegorekb <bartlomiejgrzegorek@gmail.com>
2021-06-05 10:36:19 +0200

    resource handler restructure WIP

--- pkg/project-handler.go
+++ pkg/project-handler.go
@@ -2,22 +2,63 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	"github.com/grafana/grafana-starter-datasource-backend/pkg/model"
 )
 
 type ProjectHandler struct {
 	cassandraClient *CassandraClient
 }
 
-//func (p ProjectHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-func (p ProjectHandler) CallResource(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+func (p *ProjectHandler) CallResource(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
 	log.DefaultLogger.Info("Resource Request: " + fmt.Sprintf("%s %s", request.Method, request.Path))
-	switch request.URL {
-	case "projects":
-		json := `[
+
+	if request.URL == "projects" && http.MethodPost == request.Method {
+		return p.addProject(ctx, request, sender)
+	}
+
+	if request.URL == "projects" && http.MethodGet == request.Method {
+		return p.getProjects(ctx, request, sender)
+	}
+
+	if request.URL == "projects" && http.MethodPost == request.Method {
+		return p.updateProject(ctx, request, sender)
+	}
+
+	return p.notFound(ctx, request, sender)
+}
+
+func (p *ProjectHandler) addProject(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	bodyRaw := request.Body
+	log.DefaultLogger.Info("[addProject]")
+
+	project := model.Project{}
+	if err := json.Unmarshal(bodyRaw, &project); err != nil {
+		log.DefaultLogger.Error(fmt.Sprintf("[addProject] unmarshaling. Raw project: %s", string(bodyRaw)))
+		return err
+	}
+
+	return sender.Send(
+		&backend.CallResourceResponse{
+			Status: http.StatusCreated,
+			Body: []byte(`{
+				"name":` + project.Name + `
+			}`),
+		},
+	)
+}
+
+func (p *ProjectHandler) updateProject(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	return nil
+}
+
+func (p *ProjectHandler) getProjects(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	responseRaw := `[
 			{
 			  "name": "sbc1_malmo",
 			  "title": "Brf Benzelius",
@@ -47,34 +88,28 @@ func (p ProjectHandler) CallResource(ctx context.Context, request *backend.CallR
 			  "subsystems": []
 			}
 		  ]`
-		//written, err := writer.Write([]byte(json))
-		response := backend.CallResourceResponse{
-			Status:  200,
-			Headers: make(map[string][]string),
-			Body:    []byte(json),
-		}
-		err := sender.Send(&response)
-		if err != nil {
-			log.DefaultLogger.Error("Unable to write projects to client.")
-			return err
-		}
-		log.DefaultLogger.Info("Projects sent to client.")
-	case "newProject":
-		break
-	case "newSubsystem":
-		break
-	case "newDatapoint":
-		break
-	default:
-		log.DefaultLogger.Error("Resource " + request.URL + " not found.")
-		//writer.WriteHeader(404)
-		response := backend.CallResourceResponse{
-			Status:  404,
-			Headers: make(map[string][]string),
-			Body:    []byte("Not found"),
-		}
-		err := sender.Send(&response)
+
+	err := sender.Send(&backend.CallResourceResponse{
+		Status:  http.StatusOK,
+		Headers: make(map[string][]string),
+		Body:    []byte(responseRaw),
+	})
+
+	if err != nil {
+		log.DefaultLogger.Error("Unable to write projects to client.")
 		return err
 	}
+
+	log.DefaultLogger.Info("Projects sent to client.")
 	return nil
+}
+
+func (p *ProjectHandler) notFound(ctx context.Context, request *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	log.DefaultLogger.Error("Resource " + request.URL + " not found.")
+
+	return sender.Send(&backend.CallResourceResponse{
+		Status:  404,
+		Headers: make(map[string][]string),
+		Body:    []byte("Not found"),
+	})
 }
