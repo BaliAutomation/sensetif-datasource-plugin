3e3f9258cf55c59724690aef657aeaf56ab5db6b
niclas <niclas@hedhman.org>
2022-01-09 12:11:37 +0100

    Restoring previous datasource.ts

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,21 +1,9 @@
-import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
-import { DataSourceWithBackend, getTemplateSrv, TemplateSrv } from '@grafana/runtime';
+import { DataSourceInstanceSettings } from '@grafana/data';
+import { DataSourceWithBackend } from '@grafana/runtime';
 import { SensetifDataSourceOptions, SensetifQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<SensetifQuery, SensetifDataSourceOptions> {
-  constructor(
-    instanceSettings: DataSourceInstanceSettings<SensetifDataSourceOptions>,
-    private readonly templateSrv: TemplateSrv = getTemplateSrv()
-  ) {
+  constructor(instanceSettings: DataSourceInstanceSettings<SensetifDataSourceOptions>) {
     super(instanceSettings);
-  }
-
-  applyTemplateVariables(query: SensetifQuery, scopedVars: ScopedVars): Record<string, any> {
-    return {
-      ...query,
-      project: this.templateSrv.replace(query.project, scopedVars),
-      subsystem: this.templateSrv.replace(query.subsystem, scopedVars),
-      datapoint: this.templateSrv.replace(query.datapoint, scopedVars),
-    };
   }
 }
