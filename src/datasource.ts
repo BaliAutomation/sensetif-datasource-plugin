67a08599483d7e5365e8a00e4baadefe386b1b53
niclas <niclas@hedhman.org>
2022-01-09 12:23:45 +0100

    Gently bringing across the actual code needed.

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,19 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { SensetifDataSourceOptions, SensetifQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<SensetifQuery, SensetifDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<SensetifDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: SensetifQuery, scopedVars: ScopedVars): Record<string, any> {
+    let templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      project: templateSrv.replace(query.project, scopedVars),
+      subsystem: templateSrv.replace(query.subsystem, scopedVars),
+      datapoint: templateSrv.replace(query.datapoint, scopedVars),
+    };
   }
 }
