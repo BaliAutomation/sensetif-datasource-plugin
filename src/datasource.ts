03fddbd85517de319affcfc593f2890b09337cf7
grzegorekb <bartlomiejgrzegorek@gmail.com>
2022-03-28 23:21:48 +0200

    merge fix

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,21 +1,19 @@
 import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
-import { DataSourceWithBackend, getTemplateSrv, TemplateSrv } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { SensetifDataSourceOptions, SensetifQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<SensetifQuery, SensetifDataSourceOptions> {
-  constructor(
-    instanceSettings: DataSourceInstanceSettings<SensetifDataSourceOptions>,
-    private readonly templateSrv: TemplateSrv = getTemplateSrv()
-  ) {
+  constructor(instanceSettings: DataSourceInstanceSettings<SensetifDataSourceOptions>) {
     super(instanceSettings);
   }
 
   applyTemplateVariables(query: SensetifQuery, scopedVars: ScopedVars): Record<string, any> {
+    let templateSrv = getTemplateSrv();
     return {
       ...query,
-      project: this.templateSrv.replace(query.project, scopedVars),
-      subsystem: this.templateSrv.replace(query.subsystem, scopedVars),
-      datapoint: this.templateSrv.replace(query.datapoint, scopedVars),
+      project: templateSrv.replace(query.project, scopedVars),
+      subsystem: templateSrv.replace(query.subsystem, scopedVars),
+      datapoint: templateSrv.replace(query.datapoint, scopedVars),
     };
   }
 }
