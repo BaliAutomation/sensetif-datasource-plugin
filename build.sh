53d0a0fdd65092dabb0a15b14b710e0a4d0f4421
niclas <niclas@hedhman.org>
2022-04-07 12:46:38 +0200

    Newer version

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ VERSION=DEV
 rm -rf dist 2>/dev/null
 #go build -ldflags "-linkmode external -extldflags -static" -o ./dist/gpx_sensetif-datasource_linux_amd64 ./pkg
 go build -o ./dist/gpx_sensetif-datasource_linux_amd64 ./pkg
-yarn build || exit 1
+yarn build --preserveConsole || exit 1
 
 export GRAFANA_API_KEY=eyJrIjoiMzkwNTNkZTgxZTA4ODBjY2Q2YTIwNzg1NzBjZDAyOTNjOGNkZDU3OCIsIm4iOiJQdWJsaXNoIEtleSIsImlkIjo0OTA0MDZ9
 npx @grafana/toolkit plugin:sign --rootUrls "https://sensetif.net/,http://localhost:3000/"
