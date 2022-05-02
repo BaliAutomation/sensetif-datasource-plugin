bbc6cf6cbabdd0eab95011c40192389e8ee57678
niclas <niclas@hedhman.org>
2022-05-02 12:50:39 +0200

    Enable seek() again.

--- pkg/streaming/notifications.go
+++ pkg/streaming/notifications.go
@@ -1,44 +1,45 @@
 package streaming
 
 import (
-	"context"
-	"fmt"
-	"github.com/BaliAutomation/sensetif-datasource/pkg/model"
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	"strconv"
+    "context"
+    "fmt"
+    "github.com/BaliAutomation/sensetif-datasource/pkg/model"
+    "github.com/grafana/grafana-plugin-sdk-go/backend"
+    "github.com/grafana/grafana-plugin-sdk-go/backend/log"
+    "strconv"
+    "time"
 )
 
 func (h *StreamHandler) RunNotificationsStream(ctx context.Context, req *backend.RunStreamRequest, sender *backend.StreamSender, orgId int64) error {
-	// It is Ok to send one value in each frame, since there shouldn't be too many arriving, as that indicates misconfigured
-	// system and it lies in people's own interest to fix those. However, this could be revisited in future and sending
-	// batches of errors.
-	reader := h.pulsar.CreateReader(model.NotificationTopics + strconv.FormatInt(orgId, 10))
-	defer reader.Close()
-	log.DefaultLogger.Info("Created Pulsar Reader.")
-	////oneHourAgo := time.Now().Add(60 * time.Minute)
-	////seekError := reader.SeekByTime(oneHourAgo)
-	//if seekError != nil {
-	//	log.DefaultLogger.Error(fmt.Sprintf("Unable to seek one hour back: %+v", seekError))
-	//}
-	for {
-		// The provided Context is capturing the connection back to the browser, so when it is
-		// Done(), then we should just exit the for loop.
-		msg, err := reader.Next(ctx)
-		if msg == nil {
-			log.DefaultLogger.Info("Grafana sender: DONE")
-			return ctx.Err()
-		}
-		if err != nil {
-			log.DefaultLogger.Error(fmt.Sprintf("Couldn't get the message via reader.Next(): %+v", err))
-			continue
-		}
+    // It is Ok to send one value in each frame, since there shouldn't be too many arriving, as that indicates misconfigured
+    // system and it lies in people's own interest to fix those. However, this could be revisited in future and sending
+    // batches of errors.
+    reader := h.pulsar.CreateReader(model.NotificationTopics + strconv.FormatInt(orgId, 10))
+    defer reader.Close()
+    log.DefaultLogger.Info("Created Pulsar Reader.")
+    oneHourAgo := time.Now().Add(60 * time.Minute)
+    seekError := reader.SeekByTime(oneHourAgo)
+    if seekError != nil {
+        log.DefaultLogger.Error(fmt.Sprintf("Unable to seek one hour back: %+v", seekError))
+    }
+    for {
+        // The provided Context is capturing the connection back to the browser, so when it is
+        // Done(), then we should just exit the for loop.
+        msg, err := reader.Next(ctx)
+        if msg == nil {
+            log.DefaultLogger.Info("Grafana sender: DONE")
+            return ctx.Err()
+        }
+        if err != nil {
+            log.DefaultLogger.Error(fmt.Sprintf("Couldn't get the message via reader.Next(): %+v", err))
+            continue
+        }
 
-		log.DefaultLogger.Info("Sending notification to " + req.PluginContext.User.Login)
-		err = sender.SendJSON(msg.Payload())
-		if err != nil {
-			log.DefaultLogger.Error(fmt.Sprintf("Couldn't send frame: %v", err))
-			return err
-		}
-	}
+        log.DefaultLogger.Info("Sending notification to " + req.PluginContext.User.Login)
+        err = sender.SendJSON(msg.Payload())
+        if err != nil {
+            log.DefaultLogger.Error(fmt.Sprintf("Couldn't send frame: %v", err))
+            return err
+        }
+    }
 }
